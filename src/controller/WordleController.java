3988603be4b2daf7562bb5ddd168b7c4b1b371bd
AlFarabi <muhtasimalfarabi@email.arizona.edu>
2022-03-09 23:10:49 -0800

    First commit

--- src/controller/WordleController.java
+++ src/controller/WordleController.java
@@ -1,17 +1,95 @@
 package controller;
 
+import utilities.Guess;
+import utilities.INDEX_RESULT;
+import model.WordleModel;
+
+// TODO Transfer getprogress to model
+
 public class WordleController {
 	
+	private WordleModel model;
+	/**
+	 * Allowed number of guessed
+	 */
+	public final int ALLOWEDNUMBEROFGUESSES = 6;
+	/**
+	 * Allowed length of a word
+	 */
+	public final int LEN = 5;
+	private Guess[] progress;
+	private int curr;
+	private boolean isGameOverBool;
+	
+	/**
+	 * Constructor
+	 * <p>
+	 * This method is the constructor for this class. It sets each element of the progress
+	 * array to null and assigns relevant values to other fields
+	 * @param model an instance of WordleModel class
+	 */
+	
 	public WordleController (WordleModel model) {
+		this.model = model;
+		this.curr = 1;
+		this.isGameOverBool = false;
+		this.progress = new Guess[this.ALLOWEDNUMBEROFGUESSES];
+		for(int i = 0; i < this.ALLOWEDNUMBEROFGUESSES; i++) {
+				progress[i] = null;
+		}
 	} 
 	
+	/**
+	 * Determines the game's completion
+	 * <p>
+	 * This determines whether the game is over yet or not. We modify the field 
+	 * isGameOverBool in another method
+	 * @return returns the field called isGameOverBool
+	 */
+	
 	public boolean isGameOver() {
+		return this.isGameOverBool;
 	}
+	
+	/**
+	 * Gets the answer
+	 * <p>
+	 * This method gets the answer from the model
+	 * @return a string from the model that is the actual answer word
+	 */
 	
 	public String getAnswer() {
+		return this.model.getAnswer();
 	}
 	
+	/**
+	 * Takes a guess and stores into the progress
+	 * <p>
+	 * This method calls the makeGuess method from the model and returns the output of the method
+	 * @param guess an instance of Guess, contains information about the current guess
+	 * @return returns a guess using the given parameter
+	 * @throws OnlyLettersException if guess contains non-alphabet letter
+	 * @throws NotInDictionaryException if guess is not in the Dictionary.txt file
+	 * @throws CorrectLengthException if guess is not equal to the allowed Length
+	 */
+	
 	public void makeGuess(String guess) {
+		//if(guess.length() != LEN)
+			//throw new CorrectLengthException("Invalid input. The word should be "+ LEN + " letters long.");
+		//if(!guess.matches("[a-zA-Z]+"))
+			//throw new OnlyLettersException("Invalid input. Must only contain alphabets");
+		//if(!model.getDict().contains(guess))
+			//throw new NotInDictionaryException("Invalid input. The word is not in the dictionary.");
+		
+		
+		if(model.getAnswer().equals(guess) || this.curr == ALLOWEDNUMBEROFGUESSES)
+			this.isGameOverBool = true;
+		Guess g = model.makeGuess(guess);
+		if (g.getIsCorrect()) this.isGameOverBool = true;
+		this.progress[this.curr-1] = g;
+		this.curr++;
 	}
+	
+	
 
 }
--- src/model/WordleModel.java
+++ src/model/WordleModel.java
@@ -1,25 +1,128 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import utilities.Guess;
 import utilities.INDEX_RESULT;
+/**
+ * 
+ * This class is the model class for the Wordle program. It has methods to shape the
+ * structure of the program. It is an improved version of the wordle program written
+ * for project 1 as it approaches problem by using utility classes such as Guess.java and
+ * INDEX_RESULT.java It keeps track of the correctly guessed, 
+ * correctly guessed but in the wrong place, and wrongly guessed letters.
+ * It also updates the guessed characters. This class primarily communicates with the
+ * controller class to serve the view class. We only use model inside the controller
+ * as the controller handles of the game logic. 
+ * 
+ * @author Muhtasim Al-Farabi, Tyler Conklin
+ */
 
 public class WordleModel {
 	
 	private static final String FILENAME = "dictionary.txt";
+	private static final int ALPHABETCOUNT = 26;
+	private static final int LEN = 5;
+	private String answer;
+	private INDEX_RESULT[] guessedCharacters;
+	private ArrayList<String> dictionary = new ArrayList<String>();
 
+	/**
+	 * Constructor
+	 * <p>
+	 * This is the constructor for the WordleModel class. It sets all the elements
+	 * of the guessedCharacters array to null. Then it opens up the file, adds the words into an ArrayList,
+	 * then generates a random number between 0 and that number.
+	 * Then, it selects the word at that random index and assigns as the answer.
+	 */
+	
 	public WordleModel() { 
+		try{
+			this.guessedCharacters = new INDEX_RESULT[ALPHABETCOUNT];
+			for(int i = 0; i < ALPHABETCOUNT; i++){
+				this.guessedCharacters[i] = null;
+			}
+			Scanner sc = new Scanner(new File(FILENAME));
+			while(sc.hasNext()){
+				String temp = sc.next();
+				this.dictionary.add(temp);
+			}
+			sc.close();
+			Random rand = new Random();
+			int randomNumber = rand.nextInt(dictionary.size());
+			this.answer = dictionary.get(randomNumber).toUpperCase();
+			this.answer = this.answer.toUpperCase();
+
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void makeGuess(int guessNumber, String guess) {
+		INDEX_RESULT[] index = new INDEX_RESULT[LEN];
+		boolean bool = false;
+		guess = guess.toUpperCase();
+		for(int i = 0; i < LEN; i++) {
+			for(int j = 0; j < LEN; j++){
+			if (guess.charAt(i) == this.answer.charAt(j) && i == j )
+				if (index[i] == null) index[i] = INDEX_RESULT.CORRECT;
+			}
+		}
+			
+		
+		for(int i = 0; i < LEN; i++) {
+			for(int j = 0; j < LEN; j++){
+			if (guess.charAt(i) == this.answer.charAt(j) && i != j) 
+				if (index[i] == null) index[i] = INDEX_RESULT.CORRECT_WRONG_INDEX;
+			}
+		}
+		
+		
+		for(int i = 0; i < index.length; i++)
+			if(index[i] == null) index[i] = INDEX_RESULT.INCORRECT;
+		
+		for(int i = 0; i < index.length; i++) 
+			if(guessedCharacters[guess.charAt(i) -'A'] != null && 
+					guessedCharacters[guess.charAt(i) -'A'].equals(INDEX_RESULT.CORRECT))
+				continue;
+			else
+				this.guessedCharacters[guess.charAt(i) - 'A'] = index[i];
+		
+		if(guess.equals(this.answer)) bool = true;
+		//Guess g = new Guess(guess, index, bool);
+		//return g;
 	}
+	
 
+	/**
+	 * Gets answer
+	 * <p>
+	 * This method gets the answer field
+	 * @return a string that is the actual answer word
+	 */
+	
 	public String getAnswer() {
+		return this.answer;
 	}
 
+	/**
+	 * Gets guessed characters
+	 * <p>
+	 * This method gets the guessed characters from the 
+	 * @return an array of INDEX_RESULT that contains the guessedCharacters
+	 */
+	
 	public INDEX_RESULT[] getGuessedCharacters() {
+		return this.guessedCharacters;
 	}
 	
 	public Guess[] getProgress() {
+		// later
 	}
 
 }
