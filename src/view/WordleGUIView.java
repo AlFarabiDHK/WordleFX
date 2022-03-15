307ce6c1417a81290cdec8df189a288373cc8efe
AlFarabi <muhtasimalfarabi@email.arizona.edu>
2022-03-14 23:59:31 -0700

    Dictionary change
    Changed dictionary and added more objects to scene

--- src/view/WordleGUIView.java
+++ src/view/WordleGUIView.java
@@ -1,8 +1,13 @@
 package view;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class WordleGUIView extends Application {
@@ -23,6 +28,30 @@ public class WordleGUIView extends Application {
 		BorderPane window = new BorderPane();
 		Scene scene = new Scene(window, SCENE_SIZE, SCENE_SIZE);
 		stage.setTitle("Wordle");
+		
+		String input = "";
+		/*
+		KeyEvent ke = new KeyEvent(null, null, null, null, false, false, false, false);
+		if (ke.getCode().equals(KeyCode.DELETE) || ke.getCode().equals(KeyCode.BACK_SPACE)) {
+			
+		}
+		
+		else if(ke.getCode().equals(KeyCode.ENTER)) {
+			
+		}
+		
+		else {
+			input = ke.getCode().getName();
+		}
+		*/
+		GridPane gridTop = new GridPane();
+		GridPane gridBottom = new GridPane();
+		String[] keyboardList = {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"
+								};
+		Label label = new Label(input);
+		gridTop.add(label, 0, 0);
+		
+		scene.setOnKeyPressed(null);
 		stage.setScene(scene);
 		stage.show();
 	}
